a24f44a5c68b60af6c4a958e7efb089612f2605b
Koi1zz <shugede123@gmail.com>
2026-04-24 18:28:41 +0700

    add UCtable

---
+++ my own project/UCTable.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace my_own_project
+{
+    partial class UCTable
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            this.cipStatus = new Guna.UI2.WinForms.Guna2Chip();
+            this.picTable = new System.Windows.Forms.PictureBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lblCapacity = new System.Windows.Forms.Label();
+            this.btnConfirm = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTable)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // guna2Panel1
+            // 
+            this.guna2Panel1.BorderRadius = 15;
+            this.guna2Panel1.Controls.Add(this.btnConfirm);
+            this.guna2Panel1.Controls.Add(this.lblCapacity);
+            this.guna2Panel1.Controls.Add(this.lblSize);
+            this.guna2Panel1.Controls.Add(this.lblName);
+            this.guna2Panel1.Controls.Add(this.cipStatus);
+            this.guna2Panel1.Controls.Add(this.picTable);
+            this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guna2Panel1.FillColor = System.Drawing.Color.White;
+            this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
+            this.guna2Panel1.Name = "guna2Panel1";
+            this.guna2Panel1.Size = new System.Drawing.Size(200, 150);
+            this.guna2Panel1.TabIndex = 0;
+            // 
+            // cipStatus
+            // 
+            this.cipStatus.AutoRoundedCorners = true;
+            this.cipStatus.BackColor = System.Drawing.Color.Transparent;
+            this.cipStatus.BorderRadius = 24;
+            this.cipStatus.BorderThickness = 0;
+            this.cipStatus.Font = new System.Drawing.Font("Segoe UI", 9.5F);
+            this.cipStatus.ForeColor = System.Drawing.Color.White;
+            this.cipStatus.Location = new System.Drawing.Point(115, 3);
+            this.cipStatus.Name = "cipStatus";
+            this.cipStatus.Size = new System.Drawing.Size(82, 50);
+            this.cipStatus.TabIndex = 1;
+            this.cipStatus.Text = "guna2Chip1";
+            // 
+            // picTable
+            // 
+            this.picTable.BackColor = System.Drawing.Color.White;
+            this.picTable.Location = new System.Drawing.Point(3, 3);
+            this.picTable.Name = "picTable";
+            this.picTable.Size = new System.Drawing.Size(106, 70);
+            this.picTable.TabIndex = 0;
+            this.picTable.TabStop = false;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.Transparent;
+            this.lblName.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.ForeColor = System.Drawing.Color.Black;
+            this.lblName.Location = new System.Drawing.Point(33, 27);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(45, 17);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "label1";
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.BackColor = System.Drawing.Color.Transparent;
+            this.lblSize.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.ForeColor = System.Drawing.Color.Black;
+            this.lblSize.Location = new System.Drawing.Point(3, 124);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(45, 17);
+            this.lblSize.TabIndex = 2;
+            this.lblSize.Text = "label1";
+            // 
+            // lblCapacity
+            // 
+            this.lblCapacity.AutoSize = true;
+            this.lblCapacity.BackColor = System.Drawing.Color.Transparent;
+            this.lblCapacity.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCapacity.ForeColor = System.Drawing.Color.Black;
+            this.lblCapacity.Location = new System.Drawing.Point(54, 124);
+            this.lblCapacity.Name = "lblCapacity";
+            this.lblCapacity.Size = new System.Drawing.Size(45, 17);
+            this.lblCapacity.TabIndex = 2;
+            this.lblCapacity.Text = "label1";
+            // 
+            // btnConfirm
+            // 
+            this.btnConfirm.AutoRoundedCorners = true;
+            this.btnConfirm.BackColor = System.Drawing.Color.Transparent;
+            this.btnConfirm.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnConfirm.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnConfirm.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnConfirm.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnConfirm.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnConfirm.ForeColor = System.Drawing.Color.White;
+            this.btnConfirm.Location = new System.Drawing.Point(115, 97);
+            this.btnConfirm.Name = "btnConfirm";
+            this.btnConfirm.Size = new System.Drawing.Size(82, 50);
+            this.btnConfirm.TabIndex = 3;
+            this.btnConfirm.Text = "guna2Button1";
+            // 
+            // UCTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.guna2Panel1);
+            this.Name = "UCTable";
+            this.Size = new System.Drawing.Size(200, 150);
+            this.guna2Panel1.ResumeLayout(false);
+            this.guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picTable)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
+        private System.Windows.Forms.PictureBox picTable;
+        private Guna.UI2.WinForms.Guna2Chip cipStatus;
+        private Guna.UI2.WinForms.Guna2Button btnConfirm;
+        private System.Windows.Forms.Label lblCapacity;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.Label lblName;
+    }
+}
---
+++ my own project/UCTable.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace my_own_project
+{
+    public partial class UCTable : UserControl
+    {
+        public UCTable()
+        {
+            InitializeComponent();
+
+            lblName.Parent = picTable;
+            lblName.BackColor = Color.Transparent;
+
+            lblName.Location = new Point((picTable.Width - lblName.Width) / 2, (picTable.Height - lblName.Height) / 2);
+
+            guna2Panel1.BorderRadius = 10;
+            guna2Panel1.BorderThickness = 1;
+            guna2Panel1.BorderColor = Color.FromArgb(240, 240, 240);
+            guna2Panel1.FillColor = Color.White;
+        }
+
+        public void SetTableData(string tableName, int capacity, string status, double price = 0)
+        {
+            lblName.Text = tableName;
+            lblCapacity.Text = capacity + " Person";
+
+            // Xử lý Phân loại Size dựa vào Capacity (Sức chứa)
+            if (capacity <= 2)
+            {
+                lblSize.Text = "Small";
+                // picTable.Image = Properties.Resources.icon_ban_2; // Bỏ comment khi bạn có ảnh
+            }
+            else if (capacity <= 4)
+            {
+                lblSize.Text = "Medium";
+                // picTable.Image = Properties.Resources.icon_ban_4;
+            }
+            else
+            {
+                lblSize.Text = "Large";
+                // picTable.Image = Properties.Resources.icon_ban_8;
+            }
+
+            // Căn giữa lại lblName sau khi đổi Text
+            lblName.Location = new Point((picTable.Width - lblName.Width) / 2, (picTable.Height - lblName.Height) / 2);
+
+            // Xử lý Màu sắc Trạng thái (Giống hệt hình mẫu)
+            cipStatus.Text = status;
+            if (status == "Available")
+            {
+                cipStatus.FillColor = Color.FromArgb(235, 250, 240); // Nền xanh lá nhạt
+                cipStatus.ForeColor = Color.FromArgb(40, 167, 69);   // Chữ xanh lá
+            }
+            else if (status == "Occupied")
+            {
+                cipStatus.FillColor = Color.FromArgb(255, 235, 235); // Nền đỏ nhạt
+                cipStatus.ForeColor = Color.FromArgb(220, 53, 69);   // Chữ đỏ
+            }
+            else if (status == "Reserved")
+            {
+                cipStatus.FillColor = Color.FromArgb(235, 240, 255); // Nền xanh dương nhạt
+                cipStatus.ForeColor = Color.FromArgb(0, 123, 255);   // Chữ xanh dương
+            }
+
+            // Nếu bạn muốn dùng btnConfirm để hiển thị giá tiền (giống số $126.00 trong hình)
+            btnConfirm.Text = "$" + price.ToString("N2");
+            btnConfirm.FillColor = Color.Transparent;
+            btnConfirm.ForeColor = Color.FromArgb(220, 53, 69); // Màu cam đỏ
+            btnConfirm.Font = new Font("Segoe UI", 10, FontStyle.Bold);
+        }
+
+    }
+}
