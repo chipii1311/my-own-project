669d3d6c9d4996f6effaadc6899a7a014cb5c7e8
bdpnguyen <phuocnguyen280405@gmail.com>
2026-04-24 21:49:59 +0700

    đf

--- my own project/UCFoodItem.Designer.cs
+++ my own project/UCFoodItem.Designer.cs
@@ -28,100 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.picImage = new System.Windows.Forms.PictureBox();
-            this.lblFoodName = new System.Windows.Forms.Label();
-            this.lblPrice = new System.Windows.Forms.Label();
-            this.nudQuantity = new Guna.UI2.WinForms.Guna2NumericUpDown();
-            this.btnBuy = new Guna.UI2.WinForms.Guna2Button();
-            ((System.ComponentModel.ISupportInitialize)(this.picImage)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).BeginInit();
             this.SuspendLayout();
-            // 
-            // picImage
-            // 
-            this.picImage.Dock = System.Windows.Forms.DockStyle.Top;
-            this.picImage.Location = new System.Drawing.Point(0, 0);
-            this.picImage.Name = "picImage";
-            this.picImage.Size = new System.Drawing.Size(170, 170);
-            this.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.picImage.TabIndex = 0;
-            this.picImage.TabStop = false;
-            // 
-            // lblFoodName
-            // 
-            this.lblFoodName.BackColor = System.Drawing.SystemColors.Control;
-            this.lblFoodName.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblFoodName.Location = new System.Drawing.Point(0, 170);
-            this.lblFoodName.Name = "lblFoodName";
-            this.lblFoodName.Size = new System.Drawing.Size(170, 20);
-            this.lblFoodName.TabIndex = 3;
-            this.lblFoodName.Text = "label1";
-            this.lblFoodName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // lblPrice
-            // 
-            this.lblPrice.Dock = System.Windows.Forms.DockStyle.Top;
-            this.lblPrice.ForeColor = System.Drawing.Color.Blue;
-            this.lblPrice.Location = new System.Drawing.Point(0, 190);
-            this.lblPrice.Name = "lblPrice";
-            this.lblPrice.Size = new System.Drawing.Size(170, 25);
-            this.lblPrice.TabIndex = 4;
-            this.lblPrice.Text = "label1";
-            this.lblPrice.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // nudQuantity
-            // 
-            this.nudQuantity.BackColor = System.Drawing.Color.Transparent;
-            this.nudQuantity.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.nudQuantity.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.nudQuantity.Location = new System.Drawing.Point(4, 212);
-            this.nudQuantity.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.nudQuantity.Minimum = new decimal(new int[] {
-            100,
-            0,
-            0,
-            -2147483648});
-            this.nudQuantity.Name = "nudQuantity";
-            this.nudQuantity.Size = new System.Drawing.Size(63, 33);
-            this.nudQuantity.TabIndex = 6;
-            // 
-            // btnBuy
-            // 
-            this.btnBuy.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
-            this.btnBuy.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
-            this.btnBuy.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
-            this.btnBuy.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
-            this.btnBuy.Font = new System.Drawing.Font("Segoe UI", 9F);
-            this.btnBuy.ForeColor = System.Drawing.Color.White;
-            this.btnBuy.Location = new System.Drawing.Point(71, 212);
-            this.btnBuy.Name = "btnBuy";
-            this.btnBuy.Size = new System.Drawing.Size(96, 33);
-            this.btnBuy.TabIndex = 7;
-            this.btnBuy.Text = "Thêm";
             // 
             // UCFoodItem
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.btnBuy);
-            this.Controls.Add(this.nudQuantity);
-            this.Controls.Add(this.lblPrice);
-            this.Controls.Add(this.lblFoodName);
-            this.Controls.Add(this.picImage);
             this.Name = "UCFoodItem";
             this.Size = new System.Drawing.Size(170, 250);
-            ((System.ComponentModel.ISupportInitialize)(this.picImage)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.nudQuantity)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox picImage;
-        private System.Windows.Forms.Label lblFoodName;
-        private System.Windows.Forms.Label lblPrice;
-        private Guna.UI2.WinForms.Guna2NumericUpDown nudQuantity;
-        private Guna.UI2.WinForms.Guna2Button btnBuy;
     }
 }
--- my own project/UCFoodItem.cs
+++ my own project/UCFoodItem.cs
@@ -1,79 +1,113 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Guna.UI2.WinForms;
+using System;
 using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+using System.Windows.Forms; 
 
-namespace my_own_project                
-{           
+namespace my_own_project // Nhớ đổi đúng namespace của bạn nhé
+{
     public partial class UCFoodItem : UserControl
     {
-        // Tạo một sự kiện để báo cho POSForm biết khi nào nút "Thêm" được bấm
-        public event EventHandler OnSelect = null;
+        public int FoodID { get; private set; }
+        public string FoodName { get; private set; }
+        public decimal Price { get; private set; }
 
-        // Các thuộc tính để chứa dữ liệu món ăn
-        public int FoodID { get; set; }
-        public decimal Price { get; set; }
-                
+        public event EventHandler OnSelect;
+
+        private Guna2Panel cardPanel;
+        private PictureBox picFood;
+        private Label lblName;
+        private Label lblPrice;
+        private Guna2CircleButton btnAdd;
+
         public UCFoodItem()
         {
-            InitializeComponent();
-            btnBuy.Click += btnBuy_Click;
+            // Bỏ qua InitializeComponent() vì ta tự code giao diện
+            InitializeModernCard();
         }
 
-        // Hàm này dùng để đổ dữ liệu từ SQL vào cái thẻ này
-        public void SetData(int id, string name, decimal price, string imgFileName)
+        private void InitializeModernCard()
         {
-            lblFoodName.Text = name;
+            // Kích thước thẻ và khoảng cách (Margin giúp các thẻ không bị dính vào nhau)
+            this.Size = new Size(180, 240);
+            this.BackColor = Color.Transparent;
+            this.Margin = new Padding(12);
+
+            // Tấm nền màu trắng bo góc, đổ bóng
+            cardPanel = new Guna2Panel();
+            cardPanel.Dock = DockStyle.Fill;
+            cardPanel.BorderRadius = 20;
+            cardPanel.FillColor = Color.White;
+            cardPanel.ShadowDecoration.Enabled = true;
+            cardPanel.ShadowDecoration.Depth = 10; // Bóng mờ cực xịn
+            cardPanel.ShadowDecoration.BorderRadius = 20;
+
+            // Ảnh món ăn (Canh giữa bên trên)
+            picFood = new PictureBox();
+            picFood.Size = new Size(140, 120);
+            picFood.Location = new Point(20, 15);
+            picFood.SizeMode = PictureBoxSizeMode.Zoom;
+            cardPanel.Controls.Add(picFood);
+
+            // Tên món ăn
+            lblName = new Label();
+            lblName.Font = new Font("Segoe UI Semibold", 11F);
+            lblName.Location = new Point(15, 145);
+            lblName.Size = new Size(150, 45); // Để cao xíu lỡ tên món dài rớt dòng
+            cardPanel.Controls.Add(lblName);
+
+            // Giá tiền
+            lblPrice = new Label();
+            lblPrice.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            lblPrice.Location = new Point(15, 195);
+            lblPrice.AutoSize = true;
+            cardPanel.Controls.Add(lblPrice);
+
+            // Nút Dấu Cộng (+) tròn màu đen y hệt bản thiết kế
+            btnAdd = new Guna2CircleButton();
+            btnAdd.Size = new Size(35, 35);
+            btnAdd.Location = new Point(130, 190);
+            btnAdd.FillColor = Color.FromArgb(30, 30, 30); // Đen nhám
+            btnAdd.ForeColor = Color.White;
+            btnAdd.Text = "+";
+            btnAdd.Font = new Font("Segoe UI", 14F, FontStyle.Bold);
+            btnAdd.Cursor = Cursors.Hand;
+            btnAdd.Animated = true;
+            btnAdd.HoverState.FillColor = Color.FromArgb(88, 28, 230); // Hover biến thành màu tím
+
+            // Khi click dấu + thì gọi sự kiện báo ra ngoài POSForm
+            btnAdd.Click += (s, e) => { OnSelect?.Invoke(this, EventArgs.Empty); };
+            cardPanel.Controls.Add(btnAdd);
+
+            this.Controls.Add(cardPanel);
+        }
+
+        public void SetData(int id, string name, decimal price, string imgUrl)
+        {
+            FoodID = id;
+            FoodName = name;
+            Price = price;
+
+            lblName.Text = name;
             lblPrice.Text = price.ToString("N0") + "đ";
-            this.FoodID = id;
-            this.Price = price;
 
             try
             {
-                if (!string.IsNullOrEmpty(imgFileName))
-                {
-                    // Tự động tìm thư mục MenuImages nằm cùng cấp với file .exe
-                    string fullPath = Path.Combine(Application.StartupPath, "MenuImages", imgFileName);
-
-                    if (File.Exists(fullPath))
-                    {
-                        // Giải phóng ảnh cũ nếu có để tránh tràn bộ nhớ
-                        if (picImage.Image != null) picImage.Image.Dispose();
-
-                        picImage.Image = Image.FromFile(fullPath);
-                    }
-                }
+                string imagePath = System.IO.Path.Combine(Application.StartupPath, "Images", imgUrl);
+                picFood.ImageLocation = imagePath;
             }
-            catch (Exception)
-            {
-                // Nếu lỗi hoặc không thấy ảnh, có thể hiện một ảnh mặc định
-            }
+            catch { }
         }
+
+        // --- SỬA LẠI LOGIC: KHÔNG DÙNG Ô CHỌN SỐ LƯỢNG NỮA ---
         public int GetQuantity()
         {
-            return (int)nudQuantity.Value;
+            // Thiết kế mới: Cứ bấm 1 cái dấu (+) là tự động ném 1 món vào giỏ hàng!
+            return 1;
         }
 
-        // Reset lại số lượng về 1 sau khi thêm thành công
         public void ResetQuantity()
         {
-            nudQuantity.Value = 1;
+            // Hàm này giờ bỏ trống, giữ lại để không báo lỗi bên POSForm
         }
-
-        // Khi bấm nút Thêm trên thẻ, nó sẽ kích hoạt sự kiện OnSelect
-        private void btnBuy_Click(object sender, EventArgs e)
-        {
-            if (OnSelect != null)
-                OnSelect(this, e);
-        }
-
-
     }
-
 }
