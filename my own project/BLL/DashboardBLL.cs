eee51d85f6d4b507ed735dd3028f197c6c43dce0
chipii1311 <vietnguyenhoang829@gmail.com>
2026-06-07 20:11:51 +0700

    Add more methods to DashboardBLL to support layered architecture

--- my own project/BLL/DashboardBLL.cs
+++ my own project/BLL/DashboardBLL.cs
@@ -14,5 +14,22 @@ namespace my_own_project.BLL
         public static DataTable GetRecentOrders(DateTime start, DateTime end) => DashboardDAL.GetRecentOrders(start, end);
         public static DataTable GetRevenueChart(DateTime start, DateTime end) => DashboardDAL.GetRevenueChart(start, end);
         public static DataTable GetTopProducts(DateTime start, DateTime end) => DashboardDAL.GetTopProducts(start, end);
+
+        public static DataTable GetCategoryRevenueShare(DateTime start, DateTime end)
+        {
+            return DashboardDAL.GetDataWithDateRange("sp_Dashboard_GetCategoryRevenueShare", start, end);
+        }
+
+        public static DataTable GetTop5Products(DateTime start, DateTime end)
+        {
+            try
+            {
+                return DashboardDAL.GetDataWithDateRange("sp_Dashboard_GetTop5Products", start, end);
+            }
+            catch
+            {
+                return DashboardDAL.GetDataWithDateRange("sp_Dashboard_GetTopProducts", start, end);
+            }
+        }
     }
 }
